f97c41b011a6e8f3cbea91ef2bca6f376428cfa1
Maxime Ginet <maxime.ginet@hotmail.com>
2026-04-09 11:07:07 +0200

    Création de la structure frontend React et ajout des constantes du contrat

---
+++ frontend/src/constants/contract.js
@@ -0,0 +1,17 @@
+export const ADRESSE_CONTRAT = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+
+export const ABI_CONTRAT = [
+  "function proprietaire() view returns (address)",
+  "function voteOuvert() view returns (bool)",
+  "function electeursAutorises(address) view returns (bool)",
+  "function aDejaVote(address) view returns (bool)",
+  "function obtenirNombreCandidats() view returns (uint256)",
+  "function obtenirCandidat(uint256) view returns (string, uint256)",
+  "function obtenirGagnant() view returns (string, uint256)",
+  "function ajouterElecteur(address electeur)",
+  "function ouvrirLeVote()",
+  "function voter(uint256 idCandidat)",
+  "event ElecteurAjoute(address indexed electeur)",
+  "event VoteOuvert()",
+  "event VoteEffectue(address indexed electeur, uint256 indexed idCandidat)"
+];
